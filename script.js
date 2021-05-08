f78930ed2133b4ed812c7e245757b899606dda15
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2021-05-08 13:37:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+onload = function () {
+    const editor = document.getElementById("editor");
+    const context = editor.getContext("2d");
+    const toolbar = document.getElementById("toolbar");
 
