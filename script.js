c975e346304dde1ab1bece2a1d6ee0f8e41fd3be
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2021-05-09 23:57:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,17 @@ onload = function () {
     const editor = document.getElementById("editor");
     const context = editor.getContext("2d");
     const toolbar = document.getElementById("toolbar");
+    
+    const tools = {
+        "upload" : function () {
+            const upload = document.createElement('input');
+            upload.type = "file";
+            upload.click();
+            upload.onchange = function() {
+                const img = new Image();
+                img.onload = () => {
+                    editor.width = img.width;
+                    editor.height = img.height;
+                    context.drawImage(img, 0,0);
+                };
 
